e683b516b81f1fe327da1ade4c4992fa1fcf0e72
J-Weeks <weeksjasons@gmail.com>
2015-01-22 09:33:19 -0500

    completed!

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :drums
+
+  get '/guitars/', to: 'guitars#index', as: 'guitar'
+  get '/guitars/:id', to: 'guitars#show'
+  # get '/drums/:id', to: 'drums#new'
+
 end
