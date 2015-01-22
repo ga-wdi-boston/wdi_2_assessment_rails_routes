46380c714e62f8eb8a07b923bdd05c72d92b2f22
Ashlinn Brennan <asbren13@gmail.com>
2015-01-22 09:32:19 -0500

    assessment complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,12 @@
 Rails.application.routes.draw do
+  get 'guitars', to: 'guitars#index'
+  get 'guitars/:id', to: 'guitars#show'
+  get 'drums', to: 'drums#index'
+  get 'drums/new', to: 'drums#new'
+  get 'drums/:id', to: 'drums#show'
+  get 'drums/create', to: 'drums#create'
+  post 'drums', to: 'drums#create'
+  get 'drums/:id/edit', to: 'drums#edit'
+  put 'drums/:id', to: 'drums#update'
+  delete 'drums/:id', to: 'drums#destroy'
 end
