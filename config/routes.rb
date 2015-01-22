6cca0687f959789423fc93174c0ff9e38cf5d749
idelairre <idelairre@gmail.com>
2015-01-22 09:35:58 -0500

    got all but 'routes GET /drums/new to drums#new' to pass.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,13 @@
 Rails.application.routes.draw do
+  get 'drums', to: 'drums#index'
+  get 'drums/:id', to: 'drums#show'
+  get 'drums/new', to: 'drums#new'
+  post 'drums/', to: 'drums#create'
+  delete 'drums/:id', to: 'drums#destroy' #broken
+  get 'drums/:id/edit', to: 'drums#edit'
+  put 'drums/:id', to: 'drums#update'
+
+  get 'guitars', to: 'guitars#index'
+  get 'guitars/:id', to: 'guitars#show'
+
 end
