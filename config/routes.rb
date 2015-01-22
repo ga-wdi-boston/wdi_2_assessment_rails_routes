e8345d5027c55c3819b8f153e4ec230edfade8c9
Dan Stoppelman <dstop75@gmail.com>
2015-01-22 09:36:08 -0500

    not exactly success, but not total failure?

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 Rails.application.routes.draw do
+  get '/drums', to:'drums#index'
+  get '/drums/:id', to:'drums#show', as: 'drum'
+  # get :controller'#':id, to::controller'#':action
+  # get '/patients/:id', to: 'patients#show', as: 'patient'
 end
