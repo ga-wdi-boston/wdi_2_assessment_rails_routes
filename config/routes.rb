32900b1aeec32e8fe670c05dcabef45e2e4aa917
brandonkoo <brandonkoo555@gmail.com>
2015-01-22 09:35:32 -0500

    quiz

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,18 @@
 Rails.application.routes.draw do
 end
+
+get '/guitars' , to: 'guitars#index'
+get '/guitars/1', to: 'guitars#show'
+
+get '/drums', to: 'drums#index'
+GET /drums/1 to drums#show'
+GET /drums/new to drums#new'
+POST /drums to drums#create'
+DELETE /drums/1 to drums#destroy'
+ET /drums/1/edit to drums#edit'
+PUT /drums/1 to drums#update'
+
+#get 'drums' to: 'drums'
+#end
+
+#get '/patients/:id', to: 'patients#show'
