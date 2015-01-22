d4aabd8dd4937f0da3491e02749d9dd7e87c1375
Ross <rdegnen@gmail.com>
2015-01-22 09:36:23 -0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,15 @@
 Rails.application.routes.draw do
+
+  get '/guitars' => 'guitars#index'
+  get '/guitars/1' => 'guitars#show'
+
+  get '/drums' => 'drums#index'
+  get '/drums/1' => 'drums#show'
+  get '/drums/new' => 'drums#new'
+  post '/drums/create' => 'drums#create'
+  delete '/drums/1' => 'drums#destroy'
+  get '/drums/1/edit' => 'drums#edit'
+  put '/drums/1' => 'drums#update'
+
+  resources: drums
 end
