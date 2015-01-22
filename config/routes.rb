b5f7f358a6630d944af53bb9417f51534b24f83c
Anthony Broccoli <acbrocco@syr.edu>
2015-01-22 09:44:16 -0500

    David's super slick answers, not mine

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,7 @@
 Rails.application.routes.draw do
 
-  get '/drums', to: 'drums#index'
-
-  get '/drums/:id', to: 'drums#show'
-
-  get '/drums/new', only: [:new], to: 'drums#new'
-
-  post '/drums', to: 'drums#create'
-
-  delete '/drums/:id', to: 'drums#destroy'
-
-  get '/drums/:id/edit', to: 'drums#edit'
-
-  put '/drums/:id', to: 'drums#update'
-
-  get '/guitars', to: 'guitars#index'
-
-  get '/guitars/:id', to: 'guitars#show'
-
+resources :drums
+resources :guitars, only: [:index, :show]
 
 
 
